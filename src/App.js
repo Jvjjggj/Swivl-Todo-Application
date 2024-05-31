f524dd83d8089c84d787fb67e81a550c6ba6f726
Jakeer <sjakeerhussain244@gmail.com>
2024-05-31 19:24:28 +0000

    Committing local changes before merge

--- src/App.js
+++ src/App.js
@@ -65,7 +65,11 @@ function App() {
     <div className="container App">
       <br />
       <br />
+<<<<<<< HEAD
       <h2 className="head">ToDo App</h2>
+=======
+      <h2 className='head'>To Do App</h2>
+>>>>>>> 787ad6ebd3ed5dd5d16ee1d6cacaccf6f100ae66
       <br />
       <br />
 
